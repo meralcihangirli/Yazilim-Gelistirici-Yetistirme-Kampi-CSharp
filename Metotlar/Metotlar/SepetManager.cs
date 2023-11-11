f333f4fa74bbcc6af5365adc38ab702209d5a904
Meral Cihangirli <mcihangirli@gmail.com>
2023-11-12 00:10:31 +0300

    Encapsulation
    Kapsülleme

--- Metotlar/Metotlar/SepetManager.cs
+++ Metotlar/Metotlar/SepetManager.cs
@@ -10,6 +10,7 @@ namespace Metotlar
     {
         //name convention
         //syntax
+        /*kapsulleme => Ekle metodunda kullanılan Urun classı özellikleri yenilendiginde/degistiginde (örn Urun classına +1 özellik eklendiğinde) Ekle metodunda bu özellik anında kullanılabilecektir cunkü Urun urun olarak instance edildiğinden yeni özelligi de barındırıyor olacaktır Ancak Ekle2'de ki kullanım icin yeni eklenen ozelligi kullanabilmek adına parantez icine tanımlama yapılması ve bu metodu kullanan tum alanlara da ekleme yapılması gerekecektir bu sekil kullanım dogru bir kullanım değldir.*/
         public void Ekle(Urun urun)
         {
             Console.WriteLine("Tebrikler Sepete eklendi: " + urun.Adi);
