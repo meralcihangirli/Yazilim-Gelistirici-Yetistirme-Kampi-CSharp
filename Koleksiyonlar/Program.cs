9debdbb9d0a250d78d95a0201803c6a6378c0aa4
Meral Cihangirli <mcihangirli@gmail.com>
2023-11-12 15:58:35 +0300

    Koleksiyonlar
    Koleksiyonlatt

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koleksiyonlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] isimler = new string[] {"Ahmet","Mehmet","Ayse","Fatma" };
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine(isimler[1]);
+            Console.WriteLine(isimler[2]);
+            Console.WriteLine(isimler[3]);
+            isimler = new string[5];
+            isimler[4] = "İlker";
+            Console.WriteLine(isimler[4]);
+            Console.WriteLine(isimler[0]);
+
+           
+
+        }
+    }
+}
