99fe97cece8330b3f64ac490b8c2aebf7be8b497
Meral Cihangirli <mcihangirli@gmail.com>
2023-11-12 17:20:34 +0300

    Generics
    Generics

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsIntro
+{//verilen T tipe karsılık gelir newlendiginde atanacak tip hangisi ise onu alacaktır
+    class MyList<T>
+    {
+        T[] items;
+        public void Add(T item)
+        {
+
+        }
+    }
+}
