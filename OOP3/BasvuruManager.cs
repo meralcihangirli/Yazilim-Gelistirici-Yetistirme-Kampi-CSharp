bbff631e1f4c36a6c462a26d3818a38c38afbf3d
Meral Cihangirli <mcihangirli@gmail.com>
2023-11-12 20:09:09 +0300

    Interface
    Interface

---
+++ OOP3/BasvuruManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+    class BasvuruManager
+    {
+        public void BasvuruYap(IKrediManager krediManager)
+        {
+            //Basvuran bilgilerini degerlendirme
+            krediManager.Hesapla();
+        }
+        //poliformizim
+        public void KrediOnBilgilendirmesiYap(List<IKrediManager> krediler)
+        {
+            foreach (var kredi in krediler)
+            {
+                kredi.Hesapla();
+            }
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //IhtiyacKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            //ihtiyacKrediManager.Hesapla();
+
+            //TasitKrediManager tasitKrediManager = new TasitKrediManager();
+            //tasitKrediManager.Hesapla();
+
+            //KonutKrediManager konutKrediManager = new KonutKrediManager();
+            //konutKrediManager.Hesapla();
+
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            //basvuruManager.BasvuruYap(ihtiyacKrediManager);
+            //basvuruManager.BasvuruYap(tasitKrediManager);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() { ihtiyacKrediManager,tasitKrediManager };
+            basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+
+        }
+    }
+}
