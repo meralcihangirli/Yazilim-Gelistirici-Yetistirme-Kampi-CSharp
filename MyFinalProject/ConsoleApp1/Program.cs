97cfe32aef68b854f8988bab1e59034edc578194
Meral Cihangirli <mcihangirli@gmail.com>
2023-11-14 12:36:43 +0300

    MyFinalProject
    LINQ ile veri sorgulama,Nesne Yönelimli Programlama

---
+++ MyFinalProject/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        ProductManager productManager = new ProductManager (new InMemoryProductDal());
+        foreach (var product in productManager.GetAll())
+        {
+            Console.WriteLine(product.ProductName);
+            Console.WriteLine(product.UnitPrice);
+        }
+
+    }
+}
