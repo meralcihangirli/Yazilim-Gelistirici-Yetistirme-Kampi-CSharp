e8bf55b225fbe1e75a49a8d54c6ce8f499d00089
Meral Cihangirli <148809244+meralcihangirli@users.noreply.github.com>
2023-11-19 22:30:21 +0300

    Add files via upload

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,25 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess
+{   //core katmanları diger katmanları referans almaz**
+    //generic constraint
+    //T: class=> referans tip olmalı demektir
+    //IEntity: IEntity olabilir ya da IEntitye implemente eden bir nesne de olabilir 
+    //new() : newlnebilir olmalı
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+        // List<T> GetAllByCategory(int categoryId);
+    }
+}
---
+++ Core/Utilities/Results/IDataResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public interface IDataResult<T>:IResult
+    {//Data ürünlerimiz ,kategorilerimiz ...olabilir
+     T Data { get; }
+    }
+}
