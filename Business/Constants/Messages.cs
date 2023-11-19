720c9db74ddc992531b8e5a505e829a375a20f15
Meral Cihangirli <148809244+meralcihangirli@users.noreply.github.com>
2023-11-19 22:29:03 +0300

    Add files via upload

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {//public oldugu için ProductAdded büyük harfle başladı,private olsaydı küçük harfle başlardı
+        public static string ProductAdded = "Ürün eklendi";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+        public static string MaintenanceTime = "Sistem bakımda";
+        public static string ProductListed = "Ürünler listelendi";
+    }
+}
